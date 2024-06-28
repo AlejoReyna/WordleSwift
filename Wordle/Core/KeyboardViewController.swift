d70ef105d6cf54ae27e9ea00c61bb124cfc6b901
AlejoReyna <tunnel.ovbzc@aleeas.com>
2024-06-28 14:13:12 -0600

    Keyboard appearnce configured

--- Wordle/Core/KeyboardViewController.swift
+++ Wordle/Core/KeyboardViewController.swift
@@ -23,7 +23,7 @@ class KeyboardViewController: UIViewController, UICollectionViewDelegateFlowLayo
         let collectionView = UICollectionView(frame: .zero,
                                               collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .yellow
+        collectionView.backgroundColor = .clear
         collectionView.register(KeyCell.self, forCellWithReuseIdentifier: KeyCell.identifier)
         return collectionView
     }()
@@ -36,7 +36,7 @@ class KeyboardViewController: UIViewController, UICollectionViewDelegateFlowLayo
         NSLayoutConstraint.activate([
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
         
@@ -52,32 +52,53 @@ extension KeyboardViewController {
         return keys.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection
-                        section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return keys[section].count
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt
-        indexPath: IndexPath) -> UICollectionViewCell {
+                        indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KeyCell.identifier, for: indexPath) as? KeyCell else {
             fatalError()
         }
+        let letter = keys[indexPath.section][indexPath.row]
+        cell.configure(with: letter)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout
-                        collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath:
-                        IndexPath) -> CGSize {
-        CGSize(width: 50, height: 50)
+                        collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 20
+        let size: CGFloat = (collectionView.frame.size.width-margin)/10
+        return CGSize(width: size , height: size * 1.5)
         
     }
     
     func collectionView(_ collectionView: UICollectionView, layout
                         collectionViewLayout: UICollectionViewLayout, insetForSectionAt section:
                         Int) -> UIEdgeInsets {
-                        return .zero
-    } 
+        
+        var left: CGFloat = 1
+        var right: CGFloat = 1
+        
+        let margin: CGFloat = 20
+        let size: CGFloat = (collectionView.frame.size.width-margin)/10
+        
+       
+        let count: CGFloat = CGFloat(collectionView.numberOfItems(inSection: section))
+        
+        let inset: CGFloat = (collectionView.frame.size.width - (size * count) - (2 * count))/2
+        
+        left = inset
+        right = inset
+        
+        return UIEdgeInsets(
+            top: 2,
+            left: left,
+            bottom: 2,
+            right: right)
+    }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt
 indexPath: IndexPath) {
